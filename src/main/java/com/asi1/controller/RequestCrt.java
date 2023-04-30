c387e2c581f1e6614f2522923f355f067ec2894b
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-04-30 18:42:24 +0200

    add lib + correct visuels

--- src/main/java/com/asi1/controller/RequestCrt.java
+++ src/main/java/com/asi1/controller/RequestCrt.java
@@ -5,15 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller 
+@Controller
 public class RequestCrt {
 
-	private static String messageLocal="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+	private static String messageLocal = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
 	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	public String index(Model model) {
 		model.addAttribute("messageLocal", messageLocal);
 		return "index";
 	}
-
 }
