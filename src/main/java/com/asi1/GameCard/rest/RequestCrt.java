c9aac9afe5874ddcbc52f64c66a29874c820a0fa
Mathis Gorvien <mathis.gorvien@cpe.fr>
2023-05-02 21:00:18 +0200

    web dynamique ok : create +display

--- src/main/java/com/asi1/GameCard/rest/RequestCrt.java
+++ src/main/java/com/asi1/GameCard/rest/RequestCrt.java
@@ -1,13 +1,14 @@
 package com.asi1.GameCard.rest;
 
+import com.asi1.GameCard.model.Card;
+import com.asi1.GameCard.model.CardFormDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.asi1.GameCard.model.Card;
 
 @Controller
 public class RequestCrt {
@@ -22,13 +23,26 @@ public class RequestCrt {
 
 	@GetMapping("/create-card")
 	public String showCreateCardForm(Model model) {
-		model.addAttribute("card", new Card());
-		System.out.println("coucou");
+		model.addAttribute("cardForm", new CardFormDTO());
 		return "create-card";
 	}
 
 	@PostMapping("/create-card")
-	public String createCard(Card card, Model model) {
+	public String createCard(@ModelAttribute("cardForm") CardFormDTO cardForm, Model model) {
+		// Créez un nouvel objet Card à partir des données du DTO
+		Card card = new Card();
+		card.setName(cardForm.getName());
+		card.setDescription(cardForm.getDescription());
+		card.setFamily(cardForm.getFamily());
+		card.setAffinity(cardForm.getAffinity());
+		card.setImgUrl(cardForm.getImgUrl());
+		card.setSmallImgUrl(cardForm.getSmallImgUrl());
+		card.setEnergy(cardForm.getEnergy());
+		card.setHp(cardForm.getHp());
+		card.setDefense(cardForm.getDefense());
+		card.setAttack(cardForm.getAttack());
+		card.setPrice(cardForm.getPrice());
+
 		// Simulez la sauvegarde de la carte et l'affichage des données
 		model.addAttribute("card", card);
 		return "display-card";
